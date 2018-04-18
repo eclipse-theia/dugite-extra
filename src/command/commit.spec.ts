be4ea76ed51c6f0e1a57fe4f1d51dda67be22dd9
Akos Kitta <kittaakos@gmail.com>
2018-04-18 09:23:01 +0200

    Did a minor `git commit` test clean-up.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- src/command/commit.spec.ts
+++ src/command/commit.spec.ts
@@ -14,6 +14,25 @@ describe('commit', async () => {
         track.cleanupSync();
     });
 
+    it('keeps the line breaks', async () => {
+        const path = track.mkdirSync('commit-line-breaks');
+        await initRepository(path);
+
+        fs.createFileSync(Path.join(path, 'some-file.txt'));
+        expect(fs.existsSync(Path.join(path, 'some-file.txt'))).to.be.true;
+
+        const expected = `aaaa
+
+bbbb
+
+Signed-off-by: Akos Kitta <kittaakos@gmail.com>
+`;
+        expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
+        await createCommit(path, expected);
+
+        expect((await git(['log', '--format=%B'], path, 'log')).stdout.trim()).to.be.deep.equal(expected.trim())
+    });
+
     describe('sign-off', () => {
         it('disabled', async () => {
             const path = track.mkdirSync('commit-sign-off-disabled');
@@ -29,7 +48,7 @@ describe('commit', async () => {
             expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
             await createCommit(path, 'some commit message');
 
-            expect((await git(['log', '--format=%s%b'], path, 'log')).stdout.trim()).to.not.contain(userName);
+            expect((await git(['log', '--format=%B'], path, 'log')).stdout.trim()).to.not.contain(userName);
         });
 
         it('enabled', async () => {
@@ -46,13 +65,13 @@ describe('commit', async () => {
             expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
             await createCommit(path, 'some commit message', true);
 
-            expect((await git(['log', '--format=%s%b'], path, 'log')).stdout.trim()).to.contain(userName);
+            expect((await git(['log', '--format=%B'], path, 'log')).stdout.trim()).to.contain(userName);
         });
     });
 
     describe('amend', () => {
         it('w/', async () => {
-            const path = track.mkdirSync('commit-sign-off-disabled');
+            const path = track.mkdirSync('commit-with-amend');
             await initRepository(path);
 
             fs.createFileSync(Path.join(path, 'some-file.txt'));
@@ -60,22 +79,22 @@ describe('commit', async () => {
 
             expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
             await createCommit(path, 'some commit message');
-            const beforeAmendCommits = (await git(['log', '--format=%s%b', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
+            const beforeAmendCommits = (await git(['log', '--format=%B', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
             expect(beforeAmendCommits).to.have.lengthOf(1);
-            expect(beforeAmendCommits[0].endsWith('some commit message')).to.be.true;
+            expect(beforeAmendCommits[0].trim().endsWith('some commit message')).to.be.true;
 
             fs.createFileSync(Path.join(path, 'another-file.txt'));
             expect(fs.existsSync(Path.join(path, 'another-file.txt'))).to.be.true;
 
             expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
             await createCommit(path, 'another message', false, true);
-            const afterAmendCommits = (await git(['log', '--format=%s%b', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
+            const afterAmendCommits = (await git(['log', '--format=%B', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
             expect(afterAmendCommits).to.have.lengthOf(1);
-            expect(afterAmendCommits[0].endsWith('another message')).to.be.true;
+            expect(afterAmendCommits[0].trim().endsWith('another message')).to.be.true;
         });
 
         it('w/o', async () => {
-            const path = track.mkdirSync('commit-sign-off-disabled');
+            const path = track.mkdirSync('commit-without-amend');
             await initRepository(path);
 
             fs.createFileSync(Path.join(path, 'some-file.txt'));
@@ -83,19 +102,19 @@ describe('commit', async () => {
 
             expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
             await createCommit(path, 'some commit message');
-            const beforeAmendCommits = (await git(['log', '--format=%s%b', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
+            const beforeAmendCommits = (await git(['log', '--format=%B', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
             expect(beforeAmendCommits).to.have.lengthOf(1);
-            expect(beforeAmendCommits[0].endsWith('some commit message')).to.be.true;
+            expect(beforeAmendCommits[0].trim().endsWith('some commit message')).to.be.true;
 
             fs.createFileSync(Path.join(path, 'another-file.txt'));
             expect(fs.existsSync(Path.join(path, 'another-file.txt'))).to.be.true;
 
             expect((await git(['add', '.'], path, 'add')).exitCode).to.be.equal(0);
             await createCommit(path, 'another message', false, false);
-            const afterAmendCommits = (await git(['log', '--format=%s%b', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
+            const afterAmendCommits = (await git(['log', '--format=%B', '-z'], path, 'log')).stdout.trim().split('\0').filter(c => c.trim().length > 0);
             expect(afterAmendCommits).to.have.lengthOf(2);
-            expect(afterAmendCommits[0].endsWith('another message')).to.be.true;
-            expect(afterAmendCommits[1].endsWith('some commit message')).to.be.true;
+            expect(afterAmendCommits[0].trim().endsWith('another message')).to.be.true;
+            expect(afterAmendCommits[1].trim().endsWith('some commit message')).to.be.true;
         });
     })
 
